d0463640d8789009cd405d355ed9d0b7cd762a53
Maysha-Khanom-Moon <maysha412@gmail.com>
2024-02-13 12:57:04 +0600

    rectangle complete

---
+++ scripts/rectangle.js
@@ -0,0 +1,13 @@
+function rectangleArea() {
+    const width = document.getElementById('rectanlge-width').value;
+    const length = document.getElementById('rectangle-length').value;
+
+    const area = parseFloat(width) * parseFloat(length);
+
+    document.getElementById('rectangle-area').innerText = area;
+
+    const calc = document.createElement('p');
+    calc.classList.add('text-3xl', 'mt-8');
+    calc.innerHTML = 'Rectangle Area: ' + area + 'cm<sup>2</sup>'
+    document.getElementById('sidebar').append(calc);
+}
